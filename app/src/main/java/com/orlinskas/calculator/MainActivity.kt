a27c55e0812614560bbaf35b2858bc785b55515c
orlinskas <orlinskaslove@gmail.com>
2020-02-19 00:19:37 +0200

    style

--- app/src/main/java/com/orlinskas/calculator/MainActivity.kt
+++ app/src/main/java/com/orlinskas/calculator/MainActivity.kt
@@ -6,14 +6,18 @@ import com.orlinskas.calculator.view.BottomSheetInfo
 import kotlinx.android.synthetic.main.activity_calculator.*
 
 class MainActivity : AppCompatActivity() {
-    private val steps: Array<CharSequence> = resources.getTextArray(R.array.steps)
-    private val isolation: Array<CharSequence> = resources.getTextArray(R.array.isolaion)
-    private val regulation: Array<CharSequence> = resources.getTextArray(R.array.regulation)
+    private lateinit var steps: Array<CharSequence>
+    private lateinit var isolation: Array<CharSequence>
+    private lateinit var regulation: Array<CharSequence>
     private val bottomSheetDialogFragment = BottomSheetInfo()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculator)
+
+        steps = resources.getTextArray(R.array.steps)
+        isolation = resources.getTextArray(R.array.isolaion)
+        regulation = resources.getTextArray(R.array.regulation)
 
         step_field.setValues(steps.toList())
         isolation_field.setValues(isolation.toList())
